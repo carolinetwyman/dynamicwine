8abcd2cb5e0d3d3aa0261b691d8fbfe4972b5da0
JonM <jonmisner86@gmail.com>
2020-12-01 12:58:29 -0600

    tweaks

--- client/src/pages/frontPage/frontPage.js
+++ client/src/pages/frontPage/frontPage.js
@@ -44,39 +44,44 @@ function frontPage() {
 
 export default frontPage;
 
-class frontPage extends Component {
-  state = {
-    categories: null
-  }
+
+//////////////// KEEP FOR JULIAN///////////////////////////////
+
+// class frontPage extends Component {
+//   state = {
+//     categories: null
+//   }
 
 
-  getChoices = (e) => {
-    e.preventDefault();
-    const category = e.target.elements.wineCategory.value;
-    axios.get(`https://quiniwine.com/api/pub/wineCategory/${category}/0/5?access_token=xbhysxe4p3gpeznr8kpn`)
-    .then((res) => {
-      const categories = res.data.items[0].Name + ", " + res.data.items[0].Winery + ", " + res.data.items[0].Country + ", " + res.data.items[0].id;
-      this.setState({ categories });
-      console.log(categories);
-      console.log(res);
-    })
-  }
+//   getChoices = (e) => {
+//     e.preventDefault();
+//     const category = e.target.elements.wineCategory.value;
+//     axios.get(`https://quiniwine.com/api/pub/wineCategory/${category}/0/5?access_token=xbhysxe4p3gpeznr8kpn`)
+//     .then((res) => {
+//       const categories = res.data.items[0].Name + ", " + res.data.items[0].Winery + ", " + res.data.items[0].Country + ", " + res.data.items[0].id;
+//       this.setState({ categories });
+//       console.log(categories);
+//       console.log(res);
+//     })
+//   }
 
-  render(){
-    return (
-      <div className="wine">
-      <Row>
-        <Col sm={{ size: 'auto', offset: 1 }}>
-          <Login className='center'/>,
-          <Map className='center'/>
-        </Col>
-        <Col sm={{ size: 'auto', offset: 1 }}>
-          <CategoryChoice getChoices={this.getChoices} />
-          { this.state.categories ? <p>Wine info based off your selection: {this.state.categories}</p> : <p>Please select a category</p>}
-        </Col>
-      </Row>
+//   render(){
+//     return (
+//       <div className="wine">
+//       <Row>
+//         <Col sm={{ size: 'auto', offset: 1 }}>
+//           <Login className='center'/>,
+//           <Map className='center'/>
+//         </Col>
+//         <Col sm={{ size: 'auto', offset: 1 }}>
+//           <CategoryChoice getChoices={this.getChoices} />
+//           { this.state.categories ? <p>Wine info based off your selection: {this.state.categories}</p> : <p>Please select a category</p>}
+//         </Col>
+//       </Row>
         
-      </div>
-    );
-  }
-};
+//       </div>
+//     );
+//   }
+// };
+
+///////////////////////////////////////////////////////////////////

938b79c62d500e47717715c47554b77228f5046e
JonM <jonmisner86@gmail.com>
2020-12-01 14:13:59 -0600

    latest

--- client/src/pages/bottleInfo/bottleInfo.js
+++ client/src/pages/bottleInfo/bottleInfo.js
@@ -1,14 +1,43 @@
-import React from "react";
-import "./bottleInfo.css";
+import React, { Component } from "react";
+// import axios from "axios";
 import Map from "../../components/map/map";
+import Login from '../../components/login/login';
+import Filters from '../../components/filters/filters';
+import List from '../../components/list/list';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Jumbotron, Container, Row, Col } from 'reactstrap';
+import "./bottleInfo.css";
+// import CategoryChoice from "../../components/wine-categories/category-choice";
+
 
 function bottleInfo() {
-   return (
-     <div>
-    <Map/>
-    <h1>Bottle Info Page!</h1>
-    </div>
-   );
+  return (
+    <Jumbotron fluid className="body">
+      <Row className="wine">
+          <Login className='center'/>
+      </Row>
+      <Row>
+        <Col lg={4}>
+          <Jumbotron fluid>
+            <Map/>
+          </Jumbotron>
+        </Col>
+        <Col lg={8}>
+          <Row>
+            <Jumbotron fluid>
+              <Filters/>
+            </Jumbotron>
+          </Row>
+          <Row>
+            <Jumbotron fluid>
+              <List/>
+            </Jumbotron>
+          </Row>
+        </Col>
+      </Row>
+      </Jumbotron>
+ 
+  );
  }
  
  export default bottleInfo;

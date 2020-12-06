db37164686673de87f2d6769845369ee78907b3d
JonM <jonmisner86@gmail.com>
2020-12-05 18:55:55 -0600

    dynamic markers/.env

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+//import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import frontPage from './pages/frontPage/frontPage.js';
 import bottleInfo from './pages/bottleInfo/bottleInfo.js';

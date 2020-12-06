8645c086d604e0a38642c2e0a98684ed99500b12
JonM <jonmisner86@gmail.com>
2020-12-06 00:48:51 -0600

    geocoooooodegit add -A! also testing out some styling

--- client/src/components/map/map.js
+++ client/src/components/map/map.js
@@ -8,25 +8,63 @@ import {
     Marker,
   } from "react-google-maps";
 import Geocode from "react-geocode";
+import mapStyles from "./mapStyles";
+
+const options = {
+  styles: mapStyles,
+  disableDefaultUI: true,
+  zoomControl: true,
+};
 
 const apiKey = 'https://maps.googleapis.com/maps/api/js?key=' + process.env.REACT_APP_GOOGLE_MAPS_API_KEY +'&v=3.exp&libraries=geometry,drawing,places';
 
 export default function Map() {
 
   Geocode.setApiKey(process.env.REACT_APP_GOOGLE_MAPS_API_KEY)
+  Geocode.setLanguage("en");
 
-      const wineList = [
-        {name: "Menti", vintage: 2018, coords: {lat: 45.4598, lng: 11.3401}},
-        {name: "Raventos i Blanc", vintage: 2017, coords: { lat: 41.4263208, lng: 1.7845831 }},
-        {name: "Mico Mariotti", vintage: "NV", coords: { lat: 44.6596655, lng: 11.7785897 }},
-        {name: "Ameztoi", vintage: 2019, coords: { lat: 43.2938, lng: -2.1983 }},
-        {name: "Domain Mercouri", vintage: 2017, coords: { lat: 37.75, lng: 21.58333 }}
-      ];
+  const wineList = [
+    {
+    name: "Menti", vintage: 2018, address: "UFFICI /HEADQUARTER, via Dr. Bruzzo, 24, 36053 Gambellara VI, Italy"
+    // coords: {lat: 45.4598, lng: 11.3401}
+    },
+    {
+    name: "Raventos i Blanc", vintage: 2017, address: "Plaça del Roure, S/N, 08770 Sant Sadurní d'Anoia, Barcelona, Spain"
+    // coords: { lat: 41.4263208, lng: 1.7845831 }
+    },
+    {
+    name: "Mirco Mariotti", vintage: "NV", address: "Via Rosa Bardelli, 12, 44011 Argenta FE, Italy"
+    // coords: { lat: 44.6596655, lng: 11.7785897 }
+    },
+    {
+    name: "Txakoli Ameztoi", vintage: 2019, address: "Lugar Barrio Eitzaga, 10, 20808 Eitzaga, SS, Spain",
+    // coords: { lat: 43.2938, lng: -2.1983 }
+    },
+    {
+    name: "Domaine Mercouri", vintage: 2017, address: "Korakochori 271 00, Greece",
+    // coords: { lat: 37.75, lng: 21.58333 }
+    }
+  ];
+
+  wineList.map(wine => {
+  Geocode.fromAddress(wine.address).then(
+    response => {
+      // const { lat, lng } = response.results[0].geometry.location;
+      wine.coords = response.results[0].geometry.location;
+      return (wine);
+    },
+    error => {
+      console.error(error);
+    }
+  );
+  })  
+    console.log(wineList);
 
         const MapWithAMarker = withScriptjs(withGoogleMap(props =>
             <GoogleMap className= "map"
               defaultZoom={3}
               defaultCenter={{ lat: 0, lng: 0 }}
+              options={options}
             >
             {
               wineList.map(wine => {
---
+++ client/src/components/map/mapStyles.js
@@ -0,0 +1,46 @@
+export default[
+   {
+       "stylers": [
+           {
+               "visibility": "off"
+           }
+       ]
+   },
+   {
+       "featureType": "water",
+       "stylers": [
+           {
+               "visibility": "on"
+           },
+           {
+               "color": "#2f343b"
+           }
+       ]
+   },
+   {
+       "featureType": "landscape",
+       "stylers": [
+           {
+               "visibility": "on"
+           },
+           {
+               "color": "#2b0001"
+           }
+       ]
+   },
+   {
+       "featureType": "administrative",
+       "elementType": "geometry.stroke",
+       "stylers": [
+           {
+               "visibility": "on"
+           },
+           {
+               "color": "#2f343b"
+           },
+           {
+               "weight": 1
+           }
+       ]
+   }
+];

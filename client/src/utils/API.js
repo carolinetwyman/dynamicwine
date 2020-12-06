58a7eceb963b0207095771a1491940d8316d70f6
Caroline Twyman <carolinetwyman@gmail.com>
2020-12-06 09:07:37 -0600

    removing validation on user signup for now

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -7,24 +7,24 @@ export default {
     return axios({
       method: "POST",
       data: userobject,
-      url: "/api/user/signup",
+      url: "/api/users/signup",
     });
   },
 //user logs in with an account
   userLogin: function (userobject) {
-    return axios.post("/api/user/login", userobject);
+    return axios.post("/api/users/login", userobject);
   },
 //user ends session
   userLogout: function (userobject) {
     console.log(userobject);
-    return axios.post("/api/user/logout", userobject);
+    return axios.post("/api/users/logout", userobject);
   },
 //get all wines
   getWines: function () {
-    return axios.get("/api/wine");
+    return axios.get("/api/wines");
   },
 //get wine by id
   getWine: function (id) {
-    return axios.get("/api/wine/" + id);
+    return axios.get("/api/wines/" + id);
   },
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,32 +1,35 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../../models/users');
+//const User = require('../../models/users/userModel');
 const passport = require('../../passport');
+const userController = require('../../controllers/userController');
 
-router.post('/', (req, res) => {
-    console.log('user signup');
+router.route("/signup").post(userController.userCreate);
 
-    const { username, password } = req.body;
-    //add validation
-    User.findOne({ username: username }, (err, user) => {
-        if (err) {
-            console.log('User.js post error: ', err);
-        } else if (user) {
-            res.json({
-                error: 'sorry, already a user with the username: ${username}',
-            });
-        } else {
-            const newUser = new User({
-                username: username,
-                password: password,
-            });
-            newUser.save((err, savedUser) => {
-                if (err) return res.json(err);
-                res.json(savedUser);
-            });
-        }
-    });
-});
+// router.post('/', (req, res) => {
+//     console.log('user signup');
+
+//     const { username, password } = req.body;
+//     //add validation
+//     User.findOne({ username: username }, (err, user) => {
+//         if (err) {
+//             console.log('User.js post error: ', err);
+//         } else if (user) {
+//             res.json({
+//                 error: 'sorry, already a user with the username: ${username}',
+//             });
+//         } else {
+//             const newUser = new User({
+//                 username: username,
+//                 password: password,
+//             });
+//             newUser.save((err, savedUser) => {
+//                 if (err) return res.json(err);
+//                 res.json(savedUser);
+//             });
+//         }
+//     });
+// });
 
 router.post(
     '/login',

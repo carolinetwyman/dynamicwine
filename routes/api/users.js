f8761d2bf01d41364e5431f9281991cd862bc88e
Caroline Twyman <carolinetwyman@gmail.com>
2020-12-06 09:22:33 -0600

    commenting out code so far so we can pull from userController instead of the userModel

--- routes/api/users.js
+++ routes/api/users.js
@@ -31,40 +31,40 @@ router.route("/signup").post(userController.userCreate);
 //     });
 // });
 
-router.post(
-    '/login',
-    function (req, res, next) {
-        console.log('routes/user.js, login, req.body: ');
-        console.log(req.body);
-        next();
-    },
-    passport.authenticate('local'),
-    (req, res) => {
-        console.log('logged in', req.user);
-        var userInfo = {
-            username: req.user.username,
-        };
-        res.send(userInfo);
-    }
-);
+// router.post(
+//     '/login',
+//     function (req, res, next) {
+//         console.log('routes/user.js, login, req.body: ');
+//         console.log(req.body);
+//         next();
+//     },
+//     passport.authenticate('local'),
+//     (req, res) => {
+//         console.log('logged in', req.user);
+//         var userInfo = {
+//             username: req.user.username,
+//         };
+//         res.send(userInfo);
+//     }
+// );
 
-router.get('/', (req, res, next) => {
-    console.log('====== user!! ====='),
-    console.log(req.user);
-    if (req.user) {
-        res.json({ user: req.user });
-    } else {
-        res.json({ user: null });
-    }
-});
+// router.get('/', (req, res, next) => {
+//     console.log('====== user!! ====='),
+//     console.log(req.user);
+//     if (req.user) {
+//         res.json({ user: req.user });
+//     } else {
+//         res.json({ user: null });
+//     }
+// });
 
-router.post('/logout', (req, res) => {
-    if (req.user) {
-        req.logout();
-        res.send({ msg: 'logging out' });
-    } else {
-        res.send({ msg: 'no user to log out' });
-    }
-});
+// router.post('/logout', (req, res) => {
+//     if (req.user) {
+//         req.logout();
+//         res.send({ msg: 'logging out' });
+//     } else {
+//         res.send({ msg: 'no user to log out' });
+//     }
+// });
 
 module.exports = router;
--- routes/api/wines.js
+++ routes/api/wines.js
@@ -1,4 +1,5 @@
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
 const wineController = require("../../../dynamicwine/controllers/wineController");
 
 router.route("/")

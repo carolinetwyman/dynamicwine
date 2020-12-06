48d1e717a4799f92e14e8cc21b5cddb5049b38eb
Caroline Twyman <carolinetwyman@gmail.com>
2020-12-06 07:44:32 -0600

    adding front end routes for users and wine, consolidating user.js into users.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,20 +1,30 @@
 import axios from "axios";
 
 export default {
-  // Gets all books
-  getBooks: function() {
-    return axios.get("/api/books");
+  //user creates an account 
+  //need to add needing a password
+  userSignup: function (userobject) {
+    return axios({
+      method: "POST",
+      data: userobject,
+      url: "/api/user/register",
+    });
   },
-  // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
+//user logs in with an account
+  userLogin: function (userobject) {
+    return axios.post("/api/user/login", userobject);
   },
-  // Deletes the book with the given id
-  deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
+//user ends session
+  userLogout: function (userobject) {
+    console.log(userobject);
+    return axios.post("/api/user/logout", userobject);
   },
-  // Saves a book to the database
-  saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
-  }
+//get all wines
+  getWines: function () {
+    return axios.get("/api/wine");
+  },
+//get wine by id
+  getWine: function (id) {
+    return axios.get("/api/wine/" + id);
+  },
 };

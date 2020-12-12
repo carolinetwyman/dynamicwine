53ec8aff39a63cca5da053a96d7f70ed9d2d6088
Caroline Twyman <carolinetwyman@gmail.com>
2020-12-12 13:49:41 -0600

    wines are filtered by color

--- client/src/components/filters/filters.js
+++ client/src/components/filters/filters.js
@@ -3,20 +3,38 @@ import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap
 import "./filters.css";
 
 
-export default function Filters(){
+export default function Filters({setWines, fullWineList}){
+  // const wines = props.wines
+  // const setWines = props.setWines
+  // const fullWineList = props.fullWineList
+  // const {setWines, fullWineList}
+  const handleClick = event => {
+    const color = event.target.id;
+    setWines(()=>{
+      var filteredWines = fullWineList.filter((wine)=>{
+        return wine.color === color
+      })
+      return filteredWines
+    })
+  };
+
+
 
     const [dropdownOpen, setDropdownOpen] = useState(false);
 
     const toggle = () => setDropdownOpen(prevState => !prevState);
+    
+    const handleChange= e => console.log("e.target.val",e.target.value)
+    
     return (
-        <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+        <Dropdown isOpen={dropdownOpen} toggle={toggle} onChange={handleChange}>
           <DropdownToggle caret>
             Dropdown
             </DropdownToggle>
           <DropdownMenu>
-            <DropdownItem>White</DropdownItem>
-            <DropdownItem>Rose</DropdownItem>
-            <DropdownItem>Red</DropdownItem>
+            <DropdownItem id="White" onClick={handleClick} >White</DropdownItem>
+            <DropdownItem id="Rose" onClick={handleClick} >Rose</DropdownItem>
+            <DropdownItem id="Red" onClick={handleClick} >Red</DropdownItem>
           </DropdownMenu>
         </Dropdown>
       );

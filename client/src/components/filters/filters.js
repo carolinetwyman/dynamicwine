6fd700ce8a11d1b67d0851823d420d648931ae0b
JonM <jonmisner86@gmail.com>
2020-12-12 02:57:14 -0600

    modals have arrived

--- client/src/components/filters/filters.js
+++ client/src/components/filters/filters.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./filters.css";
 
-export default class Filters extends React.Component {
+// function Filters(wines) {
+//    const [wineList, setWineList] = useState ([
+//        wines
+//    ]);
 
-    render() {
-        return(
-            <h1>
-                Filters Here
-            </h1>
-        )
-    }
+//     return (
+//       <div>
+//         {wines.filter(wine => wines.wineList.color === "Red").map(filteredWine => (
+//           <li>
+//             {filteredWine.wine_name}
+//           </li>
+//         ))}
+//       </div>
+//     );
+//   }
+  
+//   export default Filters;
 
+export default function Filters(){
+    return(
+        <h1>lets get some filters</h1>
+    )
 }

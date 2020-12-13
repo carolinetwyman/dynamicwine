4e9eac47402f0c27524ee0f55c1d4e80a23b363e
JonM <jonmisner86@gmail.com>
2020-12-12 20:22:29 -0600

    extra filter

--- client/src/components/filters/filters.js
+++ client/src/components/filters/filters.js
@@ -1,42 +1,105 @@
-import React, { useState } from 'react';
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import React, { useState } from "react";
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "reactstrap";
 import "./filters.css";
 
+export default function Filters({ setWines, fullWineList }) {
+  const [color, setColor] = useState("Color");
+  const [country, setCountry] = useState("Country");
 
-export default function Filters({setWines, fullWineList}){
-  // const wines = props.wines
-  // const setWines = props.setWines
-  // const fullWineList = props.fullWineList
-  // const {setWines, fullWineList}
-  const handleClick = event => {
+  const handleClick = (event) => {
     const color = event.target.id;
-    setWines(()=>{
-      var filteredWines = fullWineList.filter((wine)=>{
-        return wine.color === color
-      })
-      return filteredWines
-    })
+    setWines(() => {
+      var filteredWines = fullWineList.filter((wine) => {
+        return wine.color === color;
+      });
+      return filteredWines;
+    });
+    setColor(event.target.id);
   };
 
+  const handleClickCountry = (event) => {
+    const country = event.target.id;
+    setWines(() => {
+      var filteredWinesCountry = fullWineList.filter((wine) => {
+        return wine.country === country;
+      });
+      return filteredWinesCountry;
+    });
+    setCountry(event.target.id);
+  };
 
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [dropdownOpenCountry, setDropdownOpenCountry] = useState(false);
 
-    const [dropdownOpen, setDropdownOpen] = useState(false);
+  const toggle = () => setDropdownOpen((prevState) => !prevState);
+  const toggleCountry = () => setDropdownOpenCountry((prevState) => !prevState);
 
-    const toggle = () => setDropdownOpen(prevState => !prevState);
-    
-    const handleChange= e => console.log("e.target.val",e.target.value)
-    
-    return (
-        <Dropdown isOpen={dropdownOpen} toggle={toggle} onChange={handleChange}>
-          <DropdownToggle caret>
-            Dropdown
-            </DropdownToggle>
-          <DropdownMenu>
-            <DropdownItem id="White" onClick={handleClick} >White</DropdownItem>
-            <DropdownItem id="Rose" onClick={handleClick} >Rose</DropdownItem>
-            <DropdownItem id="Red" onClick={handleClick} >Red</DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-      );
-    
+  const handleChange = (e) => console.log("e.target.val", e.target.value);
+  const handleChangeCountry = (e) =>
+    console.log("e.target.val", e.target.value);
+
+  return (
+    <>
+      <Dropdown isOpen={dropdownOpen} toggle={toggle} onChange={handleChange}>
+        <DropdownToggle className="button" caret>
+          {color}
+        </DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem id="White" onClick={handleClick}>
+            White
+          </DropdownItem>
+          <DropdownItem id="Rose" onClick={handleClick}>
+            Rose
+          </DropdownItem>
+          <DropdownItem id="Red" onClick={handleClick}>
+            Red
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+
+      <Dropdown
+        isOpen={dropdownOpenCountry}
+        toggle={toggleCountry}
+        onChange={handleChangeCountry}
+      >
+        <DropdownToggle className="button" caret>
+          {country}
+        </DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem id="Canada" onClick={handleClickCountry}>
+            Canada
+          </DropdownItem>
+          <DropdownItem id="France" onClick={handleClickCountry}>
+            France
+          </DropdownItem>
+          <DropdownItem id="Germany" onClick={handleClickCountry}>
+            Germany
+          </DropdownItem>
+          <DropdownItem id="Greece" onClick={handleClickCountry}>
+            Greece
+          </DropdownItem>
+          <DropdownItem id="Italy" onClick={handleClickCountry}>
+            Italy
+          </DropdownItem>
+          <DropdownItem id="New Zealand" onClick={handleClickCountry}>
+            New Zealand
+          </DropdownItem>
+          <DropdownItem id="Portugal" onClick={handleClickCountry}>
+            Portugal
+          </DropdownItem>
+          <DropdownItem id="Spain" onClick={handleClickCountry}>
+            Spain
+          </DropdownItem>
+          <DropdownItem id="USA" onClick={handleClickCountry}>
+            USA
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+    </>
+  );
 }

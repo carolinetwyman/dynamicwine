e468dbab3036cdf6d2a29df56f55b053fb1d6ba2
joolyenIV <julianivey@gmail.com>
2020-12-16 16:20:56 -0600

    adding passport - dependencies added and passport.js file added for middleware, routes and to start server

---
+++ client/passport.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+const passportLocal = require("passport-local").Strategy;
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcryptjs");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+//Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors({
+    origin: "http://localhost:3000", //  <--- React app connection location
+    credentials: true
+}));
+app.use(session({
+    secret: "secretcode",
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(cookieParser("secretcode"))
+
+//Routes
+app.post("/login", (req,res) => {
+    console.log(req.body);
+});
+app.post("/register", (req,res) => {
+    console.log(req.body);
+});
+app.get("/user", (req,res) => {});
+
+//Start Server
+app.listen(4000, () => {
+    console.log("Server has started");
+})
